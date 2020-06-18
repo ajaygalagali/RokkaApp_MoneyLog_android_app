59b308fafab9ccd1616c36d3cca3078cde96b7d4
Ajay <[gvajay321@gmail.com]>
2020-06-18 20:54:43 +0530

    DetailsActivity v1.1 updated

--- app/src/main/java/com/astro/rokka/DetailsActivity.java
+++ app/src/main/java/com/astro/rokka/DetailsActivity.java
@@ -25,13 +25,17 @@ import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
     SQLiteDatabase db;
-    String Name;
+    String Name, mem_id;
+
     TextView textViewHeading;
     ListView listViewDet;
-    String Time;
-    String mem_id;
-    int transfer_index;
-    int date_index;
+
+
+
+    int date_index, days_index, paid_wages_index, rem_wages_index, total_wages_index, note_index;
+    int total_wages,paid_wages, rem_wages,days;
+    String date, note;
+
     static  DetailsAdapter detailsAdapter;
 
     static ArrayList<DetailsList> detList;
@@ -47,6 +51,7 @@ public class DetailsActivity extends AppCompatActivity {
         mem_id = i.getStringExtra("mem_id");
         Name = i.getStringExtra("name");
         textViewHeading.setText(Name);
+
         detList = new ArrayList<>();
         detailsAdapter = new DetailsAdapter(this, detList);
 
@@ -55,16 +60,28 @@ public class DetailsActivity extends AppCompatActivity {
         @SuppressLint("Recycle") Cursor c = db.rawQuery(String.format("SELECT * FROM '%s'",mem_id),null);
 
         int j = c.getCount();
-        transfer_index = c.getColumnIndex("transfer");
+
         date_index = c.getColumnIndex("date");
-        Log.i("Index",String.valueOf(date_index)+" "+String.valueOf(transfer_index));
-        Log.i("Count",String.valueOf(j));
-        c.moveToFirst();
+        days_index = c.getColumnIndex("days");
+        note_index = c.getColumnIndex("note");
+        total_wages_index = c.getColumnIndex("total_wages");
+        rem_wages_index = c.getColumnIndex("rem_wages");
+        paid_wages_index = c.getColumnIndex("paid_wages");
+
+
+
+        c.moveToLast();
         for(int k=0;k<j;k++){
-            int transfer = c.getInt(transfer_index);
-            String date = c.getString(date_index);
-            detList.add(new DetailsList(transfer,date));
-            c.moveToNext();
+            total_wages = c.getInt(total_wages_index);
+            paid_wages = c.getInt(paid_wages_index);
+            rem_wages = c.getInt(rem_wages_index);
+            days = c.getInt(days_index);
+
+            date = c.getString(date_index);
+            note = c.getString(note_index);
+            //int days, int total_wages,int rem_wages,int paid_wages,String note,String time
+            detList.add(new DetailsList(days,total_wages,rem_wages,paid_wages,note,date));
+            c.moveToPrevious();
         }
 
 
@@ -100,16 +117,21 @@ public class DetailsActivity extends AppCompatActivity {
             view = LayoutInflater.from(mContext).inflate(R.layout.details_row,parent,false);
             DetailsList currentPosition = cablist.get(position);
 
-            TextView textViewNumber = view.findViewById(R.id.textViewDetNumber);
-            TextView textViewTime  = view.findViewById(R.id.textViewDetTime);
+            TextView textViewDays, textViewTotalWage, textViewPaidWage, textViewRemWage,textViewDate,textViewNote;
 
-            textViewNumber.setText(String.valueOf(currentPosition.getNumber()));
-            if(currentPosition.getNumber() < 0){
-                textViewNumber.setTextColor(Color.RED);
-            }else {
-                textViewNumber.setTextColor(Color.GREEN);
-            }
-            textViewTime.setText(String.valueOf(currentPosition.getTime()));
+            textViewDate = view.findViewById(R.id.textViewDtDate);
+            textViewDays = view.findViewById(R.id.textViewDtDays);
+            textViewNote = view.findViewById(R.id.textViewDtNote);
+            textViewPaidWage = view.findViewById(R.id.textViewDtGiven);
+            textViewRemWage = view.findViewById(R.id.textViewDtRem);
+            textViewTotalWage = view.findViewById(R.id.textViewDtTotalWage);
+
+            textViewDate.setText(currentPosition.getTime());
+            textViewDays.setText("ಆಳ : "+String.valueOf(currentPosition.getDays()));
+            textViewNote.setText(currentPosition.getNote());
+            textViewPaidWage.setText("ಕೊಟ್ಟುದ್ದು : "+String.valueOf(currentPosition.getPaid_wages()));
+            textViewRemWage.setText("ಉಳುದ್ದಿದ್ದು : "+String.valueOf(currentPosition.getRem_wages()));
+            textViewTotalWage.setText("ಪಗಾರ : "+String.valueOf(currentPosition.getTotal_wages()));
 
             return view;
         }
--- app/src/main/java/com/astro/rokka/DetailsList.java
+++ app/src/main/java/com/astro/rokka/DetailsList.java
@@ -1,27 +1,72 @@
 package com.astro.rokka;
 
 public class DetailsList {
-    private int number;
-    private String time;
 
-    public DetailsList(int number,String time){
-        this.number=number;
-        this.time=time;
+    private String time;
+    private int total_wages;
+    private int paid_wages;
+    private int days;
+    private int rem_wages;
+
+    private String note;
+
+
+    public DetailsList(int days, int total_wages,int rem_wages,int paid_wages,String note,String time){
+
+        this.time = time;
+        this.days = days;
+        this.paid_wages = paid_wages;
+        this.total_wages = total_wages;
+        this.rem_wages = rem_wages;
+        this.note = note;
     }
 
-    public int getNumber() {
-        return number;
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getTime() {
         return time;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public int getDays() {
+        return days;
     }
 
-    public void setTime(String time) {
-        this.time = time;
+    public int getPaid_wages() {
+        return paid_wages;
     }
+
+    public int getRem_wages() {
+        return rem_wages;
+    }
+
+    public int getTotal_wages() {
+        return total_wages;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public void setPaid_wages(int paid_wages) {
+        this.paid_wages = paid_wages;
+    }
+
+    public void setTotal_wages(int total_wages) {
+        this.total_wages = total_wages;
+    }
+
+    public void setRem_wages(int rem_wages) {
+        this.rem_wages = rem_wages;
+    }
+
 }
